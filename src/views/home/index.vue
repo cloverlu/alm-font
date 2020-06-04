c268102af9e375ca07d81a27acb1c6b43f308c2c
penglu <clover_lu@foxmail.com>
2020-06-04 17:21:18 +0800

    1、首页；2、贷后检查列表；3、流程1（进度10%）；4、路由的框架部署

---
+++ src/views/home/index.vue
@@ -0,0 +1,118 @@
+<!--
+ * @Descripttion: 主页
+ * @Author: penglu
+ * @Date: 2020-06-01 16:18:20
+-->
+<template lang="pug">
+	.wrapper 	
+		.tabs 
+			.tabs-card(@click="handclick")
+				.card(:class="tabsVisible ? 'active' : ''")
+					.one 待办列表
+				span |
+				.card(:class="tabsVisible ? '' : 'active'")
+					.two 我的客户
+				.card-bar(:class="tabsVisible ? 'card-bar-one' : 'card-bar-two'")
+			.tabs-wrapper
+				.wrapper-content(v-if="tabsVisible") 
+					TodoList
+				.wrapper-content(v-else-if="!tabsVisible") 
+					UserList
+		router-view 
+</template>
+
+<script>
+import TodoList from "../home/todoList";
+import UserList from "../home/userList";
+export default {
+  components: { TodoList, UserList },
+  data() {
+    return {
+      tabsVisible: true
+    };
+  },
+  methods: {
+    handclick() {
+      console.log(event);
+      this.tabsVisible = !this.tabsVisible;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/global.scss";
+.wrapper {
+  width: 100%;
+  height: 100%;
+  .tabs {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    .tabs-card {
+      width: 100%;
+      height: px2rem(44);
+      // flex: 0 0 px2rem(44);
+      border-bottom: px2rem(1) solid #d6d6db;
+      display: flex;
+      position: relative;
+      .card {
+        flex: 1;
+        line-height: px2rem(44);
+        font-size: px2rem(15);
+        font-weight: Bold;
+        color: #9f9f9f;
+        .one {
+          margin: 0 auto;
+          text-align: center;
+          width: px2rem(100);
+        }
+        .two {
+          margin: 0 auto;
+          text-align: center;
+          width: px2rem(100);
+        }
+        &:hover,
+        &:focus,
+        &.active {
+          .one {
+            color: #4e78de;
+          }
+          .two {
+            color: #4e78de;
+          }
+        }
+      }
+      .card-bar {
+        position: absolute;
+        bottom: 0;
+        width: px2rem(100);
+        height: px2rem(1);
+        background-color: #4e78de;
+        transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+        &.card-bar-one {
+          transform: translateX(px2rem(43.75));
+        }
+        &.card-bar-two {
+          transform: translateX(px2rem(231.25));
+        }
+      }
+      span {
+        font-size: px2rem(15);
+        color: #707070;
+        margin-top: px2rem(15);
+      }
+    }
+  }
+  .tabs-wrapper {
+    flex: 1;
+    width: 100%;
+    box-sizing: border-box;
+    .wrapper-content {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
---
+++ src/views/home/todoList.vue
@@ -0,0 +1,88 @@
+<!--
+ * @Descripttion: 待办列表
+ * @Author: penglu
+ * @Date: 2020-06-03 16:23:28
+-->
+
+<template lang="pug">
+	.todo-list
+		scroll(:top="46")
+			.list-item(v-for="item in todoListTitle" :key="item.id" @click="handleClick(item.id)")
+				.icon
+					span(:class="item.icon")
+				.title {{item.text}}
+				.warning-num(v-if="item.warningNumber") （{{item.warningNumber}}项）
+				.warning-num(v-else) 
+				.righht-arrow
+					span(class="iconfont iconxiala")
+</template>
+
+<script>
+import { todoListTitle } from "../../utils/dataMock.js";
+import Scroll from "../../components/Scroll";
+export default {
+  components: { Scroll },
+  data() {
+    return {
+      todoListTitle: todoListTitle
+    };
+  },
+  methods: {
+    handleClick(id) {
+      if (id === 2) {
+        this.$router.push({ name: "loanInspectionIndex" });
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/global.scss";
+.todo-list {
+  width: 100%;
+  height: 100%;
+  @include scroll;
+  .list-item {
+    width: 100%;
+    height: px2rem(44);
+    line-height: px2rem(44);
+    padding: 0 px2rem(12) 0 px2rem(18);
+    display: flex;
+    font-size: px2rem(14);
+    border-bottom: px2rem(1) solid #e5e5e5;
+    position: relative;
+    box-sizing: border-box;
+    .icon {
+      color: #858993;
+      margin-right: px2rem(5);
+      font-size: px2rem(15);
+    }
+    .title {
+      color: #090909;
+    }
+    .warning-num {
+      color: #db3822;
+    }
+    .righht-arrow {
+      color: #c6c6c6;
+      position: absolute;
+      right: px2rem(17);
+    }
+    &:hover,
+    &:focus,
+    &.active {
+      background-color: #4e78de;
+      .icon {
+        color: #fff;
+      }
+      .title {
+        color: #fff;
+      }
+      .righht-arrow {
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
